9b5359c98647ef020ceb80989d5fd58276ca35b7
Scott Block <scottrblock@gmail.com>
2022-07-13 23:00:30 -0400

    working using roamjs scripts

--- src/ptn-roam-depot.ts
+++ src/ptn-roam-depot.ts
@@ -2,31 +2,39 @@
 // import { configure, configValues } from "../configure";
 // import Bugsnag from "@bugsnag/js";
 
-import { runExtension } from "roamjs-components";
-
-// Bugsnag.start({ apiKey: "0ca67498b27bd9e3fba038f7fb0cd0b4" });
-
-// if (roamKey) {
-//   Bugsnag.setUser(roamKey, undefined, undefined);
-// }
-
-// configure();
-// fetchNotes();
-
-// document.addEventListener("click", (e: any) => {
-//   if (e?.target?.innerText?.toUpperCase() === "DAILY NOTES") {
-//     fetchNotes();
-//   }
-// });
-
-// window.setInterval(() => fetchNotes(), 1000 * 60);
-
-export default runExtension({
-  extensionId: "ptn-roam-depot",
-  run: () => {
+export default {
+  onload: () => {
     console.log("ptn log onload");
+    // Bugsnag.start({ apiKey: "0ca67498b27bd9e3fba038f7fb0cd0b4" });
+
+    // if (roamKey) {
+    //   Bugsnag.setUser(roamKey, undefined, undefined);
+    // }
+
+    // configure();
+    // fetchNotes();
+
+    // document.addEventListener("click", (e: any) => {
+    //   if (e?.target?.innerText?.toUpperCase() === "DAILY NOTES") {
+    //     fetchNotes();
+    //   }
+    // });
+
+    // window.setInterval(() => fetchNotes(), 1000 * 60);
   },
-  unload: () => {
+  onunload: () => {
     console.log("ptn log onunload");
   },
-});
+};
+
+// #TODO move to this once it works
+// export default runExtension({
+//   roamMarketplace: true,
+//   extensionId: "ptn-roam-depot",
+//   run: () => {
+//     console.log("ptn log onload");
+//   },
+//   unload: () => {
+//     console.log("ptn log onunload");
+//   },
+// });
